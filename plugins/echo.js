9d9f18bcb5b823cbd1f61d5ce56b4e3654c74352
mrzf833 <palaktais@gmail.com>
2021-05-15 10:05:40 +0700

    modifed

--- plugins/echo.js
+++ plugins/echo.js
@@ -5,7 +5,7 @@ let token = Cookie.get('token')
 window.Pusher = require('pusher-js');
         window.Echo = new Echo({
             /*  
-                setting authEndpointnya
+                setting authEndpointnya api nya
                 misalanya: http://localhost/api-chat/public/broadcasting/auth
                 atau: http://127.0.0.1:8000/broadcasting/auth
                 atau: http://api-chat.test/broadcasting/auth
