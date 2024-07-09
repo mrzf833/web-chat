52510e8c721de0aa6c6a25d185a5fa9ccdf461b1
mrzf833 <palaktais@gmail.com>
2024-07-09 15:51:54 +0700

    fix url plugins

--- plugins/echo.js
+++ plugins/echo.js
@@ -3,6 +3,9 @@ import Cookie from 'js-cookie'
 // import Process from 'dotenv'
 
 let token = Cookie.get('token')
+
+let authEndpoint = (process.env.BASE_URL || "http://localhost:8084/") + '/broadcasting/auth'
+authEndpoint = authEndpoint.replace(/([^:]\/)\/+/g, "$1")
 window.Pusher = require('pusher-js');
         window.Echo = new Echo({
             /*  
@@ -11,11 +14,13 @@ window.Pusher = require('pusher-js');
                 atau: http://127.0.0.1:8000/broadcasting/auth
                 atau: http://api-chat.test/broadcasting/auth
             */
-            authEndpoint : (process.env.BASE_URL || "http://localhost:8084/") + 'broadcasting/auth',
+            authEndpoint : authEndpoint,
             broadcaster: 'pusher',
 
             // setting data pusher. pakai petik
             key: process.env.PUSHER_KEY || '',
             cluster: process.env.PUSHER_CLUSTER || 'ap1',
             forceTLS: process.env.PUSHER_FORCE_TLS || true,
-        }) 
+        })
+
+        console.log(authEndpoint);
